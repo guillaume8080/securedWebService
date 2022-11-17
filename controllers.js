052f74c0da5cfea51df3a84e8ac97b45c0319dbb
guillaume8080 <guillaume.gouy@ynov.com>
2022-11-17 14:44:28 +0100

    one layer as function

---
+++ controllers.js
@@ -0,0 +1,11 @@
+const  recordToken = () => {
+    return "is it recording??";
+}
+
+/* const controllerToken = () => {
+    function recordToken(){
+        return "is it recording??";
+    }
+*/
+
+module.exports = recordToken()
--- entryPoint.js
+++ entryPoint.js
@@ -2,11 +2,20 @@
 var express = require('express');
 var app = express();
 
+const recordToken = require("./controllers");
+
 
 app.listen(3000)
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
     res.send('hello world');
+});
+
+app.get('/record', function(req, res) {
+
+    const retour = recordToken;
+
+    res.send(retour);
 });
 
